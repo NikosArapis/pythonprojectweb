b2161859ca1488a79cf38a2d61cce9e46d72a07c
NikosArapis <yiotatsakiridi@gmail.com>
2024-04-15 19:08:59 +0200

    commit 2

--- about.py
+++ about.py
@@ -4,7 +4,7 @@ def app():
     st.write("Laniteio School, nestled in the serene environment of Knowledge City, stands as a beacon of progressive education and holistic development. Established in 1995, the school has earned a reputation for providing a robust educational experience that seeks to cultivate the innate potential of each student.")
     # Display the image
     st.image("school.png", caption="School in forest", use_column_width=True)
-    with st.expander("See Philosophy:"):
+    with st.expander("See more details"):
         st.write(
             "The philosophy of Greenwood is deeply rooted in the belief that education should extend beyond academic excellence to include the development of character, creativity, and curiosity in every child. The school aims to foster an environment where students are encouraged to explore, inquire, and innovate, enabling them to become lifelong learners and contributors to a global society.")
     # Create three columns
